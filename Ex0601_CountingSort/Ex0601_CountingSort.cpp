3ff32d2820805b37bc0dfd2f0bf9b96a175998b0
Franordin <98395260+Franordin@users.noreply.github.com>
2024-09-22 19:09:05 +0900

    day6

--- Ex0601_CountingSort/Ex0601_CountingSort.cpp
+++ Ex0601_CountingSort/Ex0601_CountingSort.cpp
@@ -20,34 +20,35 @@ void Print(vector<int>& arr)
 
 // 입력은 0 이상 k 이하의 정수, CSLR 8.2
 // 결과를 반환하는 구조 (In-place가 아님)
-vector<int> CountingSort(const vector<int>& arr, int k)
-{
+vector<int> CountingSort(const vector<int>& arr, int k) {
 	vector<int> count(k + 1, 0); // 0이상 k이하니까 k + 1개를 0으로 초기화
 
-	// TODO:
+	for (auto a : arr)
+		count[a]++;
+	for (int i = 1; i < count.size(); i++)
+		count[i] += count[i - 1];
 
 	cout << "Count: ";
 	Print(count);
-
+	
 	vector<int> output(arr.size(), -1); // -1로 초기화하는 것은 디버깅용
-
+	
 	// 역순으로 복사
-	for (int i = output.size() - 1; i >= 0; i--)
-	{
-		// TODO:
-
+	for (int i = output.size() - 1; i >= 0; i--) {
+		count[arr[i]]--;
+		output[count[arr[i]]] = arr[i];
+	
 		cout << "Count: ";
 		Print(count);
-
+	
 		cout << "Output: ";
 		Print(output);
 	}
-
+	
 	return output;
 }
 
-int main()
-{
+int main() {
 	vector<int> arr = { 2, 5, 3, 0, 2, 3, 0, 3 };
 
 	Print(arr);
@@ -58,6 +59,6 @@ int main()
 	vector<int> result = CountingSort(arr, k);
 
 	Print(result);
-
+	
 	return 0;
 }
--- Ex0602_RadixSort/Ex0602_RadixSort.cpp
+++ Ex0602_RadixSort/Ex0602_RadixSort.cpp
@@ -1,15 +1,14 @@
-﻿#include <iostream>
-#include <vector>
-#include <random>
-#include <limits>
+﻿#include <chrono>
 #include <fstream>
-#include <chrono>
-#include <numeric>
+#include <iostream>
 #include <iomanip>
+#include <limits>
+#include <numeric>
+#include <random>
+#include <vector>
 using namespace std;
 
-void Print(vector<int>& arr)
-{
+void Print(vector<int>& arr) {
 	for (auto& a : arr)
 		cout << a << " ";
 	cout << endl;
@@ -18,32 +17,42 @@ void Print(vector<int>& arr)
 // 편의상 결과가 arr에 저장되도록 바꿨습니다.
 void CountingSort(vector<int>& arr, int k, int exp)
 {
-	vector<int> temp = arr; // 복사
+    vector<int> temp = arr; // 복사
 
-	vector<int> count(k + 1, 0);
-	// TODO:
+    vector<int> count(k + 1, 0);
+    for (auto a : temp) count[a / exp % 10] += 1;
+    for (int i = 1; i < count.size(); i++)
+        count[i] += count[i - 1];
 
-	for (int i = arr.size() - 1; i >= 0; i--)
-	{
-		// TODO:
-	}
+    Print(count);
+
+    for (int i = arr.size() - 1; i >= 0; i--)
+    {
+        count[temp[i] / exp % 10]--;
+        arr[count[temp[i] / exp % 10]] = temp[i];
+
+        cout << "Count: ";
+        Print(count);
+
+        cout << "Output: ";
+        Print(arr);
+    }
 }
+
 
 void RadixSort(vector<int>& arr)
 {
-	int k = 9; // 0 이상 9 이하
-	int m = *max_element(arr.begin(), arr.end());
+    int k = 9;
+    int m = *max_element(arr.begin(), arr.end());
 
-	//for (TODO)
-	//{
-	//	cout << "exp = " << exp << endl;
-	//	TODO:
-	//	Print(arr);
-	//}
+    for (int exp = 1; m / exp > 0; exp *= 10)
+    {
+        cout << "exp = " << exp << endl;
+        CountingSort(arr, k, exp);
+    }
 }
 
-int main()
-{
+int main() {
 	vector<int> arr = { 170, 45, 75, 90, 802, 24, 2, 66 };
 	//vector<int> arr = { 2, 5, 3, 0, 2, 3, 0, 3 };
 
