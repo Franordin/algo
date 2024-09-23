1d7cbd9d3968f76fc784dd1ebaf331cd03682b9e
Son Hyeon Tae <104341400+Son-HyeonTae@users.noreply.github.com>
2024-09-23 21:25:37 +0900

    day7

--- Ex0603_SortingWords/Ex0603_SortingWords.cpp
+++ Ex0603_SortingWords/Ex0603_SortingWords.cpp
@@ -10,32 +10,62 @@
 using namespace std;
 
 template<typename T>
-void Print(vector<T>& arr)
-{
+void Print(vector<T>& arr) {
 	for (auto& a : arr)
 		cout << a << " ";
 	cout << endl;
 }
 
-void CountingSort(vector<string>& arr, int k, int d)
-{
-	// vector<string> temp = arr; // 복사
-	// std::fill(arr.begin(), arr.end(), "   "); // 디버깅 편의
+void CountingSort(vector<string>& arr, int k, int d) {
+	vector<string> temp = arr; // 복사
+	//std::fill(arr.begin(), arr.end(), "   "); // 디버깅 편의
 
-	// TODO:
+	vector<int> count(k, 0);
+	for (int i = 0; i < temp.size(); i++) {
+		//cout << temp[i][d] << " ";
+		count[temp[i][d] - 'A']++;
+	}
+	for (int i = 1; i < count.size(); i++)
+		count[i] += count[i - 1];
+
+	for (int i = temp.size() - 1; i >= 0; i--) {
+		count[temp[i][d] - 'A']--;
+		arr[count[temp[i][d] - 'A']] = temp[i];
+
+		cout << "Count: ";
+		Print(count);
+
+		cout << "Output: ";
+		Print(arr);
+	}
 }
 
-void RadixSort(vector<string>& arr)
-{
-	// TODO:
+
+void RadixSort(vector<string>& arr) {
+	int k = 26;
+
+	for (int d = 2; d >= 0; d--) {
+		cout << endl;
+		for (int i = 65; i < 65 + k; i++)
+			cout << (char)i << " ";
+
+		cout << "Digit = " << d << endl;
+		cout << "k = " << k << endl;
+		CountingSort(arr, k, d);
+	}
 }
 
-int main()
-{
+int main() {
 	// CLRS 8.3-1
 	vector<string> arr = { "COW", "DOG", "SEA", "RUG",
 		"ROW", "MOB", "BOX", "TAB", "BAR", "EAR", "TAR",
 		"DIG", "BIG", "TEA", "NOW", "FOX" };
+
+	//for (int i = 0; i < arr.size(); i++) {
+	//	for (int j = 0; j < 3; j++)
+	//		cout << arr[i][j] - 65 << " ";
+	//	cout << endl;
+	//}
 
 	Print(arr);
 
